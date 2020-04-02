8dd6909be161815981f624abe46fe63c0f439791
ebubekirbastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-04-03 01:38:31 +0300

    Add project files.

---
+++ Osint Tools/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Osint_Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(363, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Taramaya Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(225, 25);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(363, 290);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Twitter \\/ Facebook";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(363, 44);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(612, 436);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Osint";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Osint Tools/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using EbubekirBastamatxtokuma;
+using OpenQA.Selenium.Chrome;
+
+namespace Osint_Tools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        string dgr = "";
+        Thread th; ChromeDriver drv; OpenFileDialog op;
+        string[] urller = { "https://twitter.com/", "https://www.facebook.com/" , "https://www.instagram.com/", "https://github.com/" };
+        private void button2_Click(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                th = new Thread(verioku); th.Start();
+            }
+        }
+        private void verioku()
+        {
+            BekraTxtOkuma.Txtİmport(op.FileName, listBox1, false);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(tara); th.Start();
+        }
+        private void tara()
+        {
+
+            drv = new ChromeDriver();
+            for (int i = 0; i < urller.Length; i++)
+            {
+                dgr = urller[i].ToString();
+                for (int j = 0; j < listBox1.Items.Count; j++)
+                {
+                    if (dgr == "https://twitter.com/")
+                    {
+                        urlac(urller[i].ToString() + listBox1.Items[j].ToString());
+                        Thread.Sleep(2000);
+                        if (drv.PageSource.IndexOf("Böyle bir hesap yok") != -1)
+                        {
+                            listBox2.Items.Add("Bu twitter Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut Değil..");
+                        }
+                        else if (drv.PageSource.IndexOf("Hesap askıya alındı") != -1)
+                        {
+                            listBox2.Items.Add("Bu twitter Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Askıya Alındı..");
+                        }
+                        else
+                        {
+                            listBox2.Items.Add("Bu twitter Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut");
+                        }
+                    }
+                    else if (dgr == "https://www.facebook.com/")
+                    {
+                        urlac(urller[i].ToString() + listBox1.Items[j].ToString());
+                        if (drv.PageSource.IndexOf("Bu sayfaya ulaşılamıyor") != -1)
+                        {
+                            listBox2.Items.Add("Bu facebook Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut Değil..");
+                        }
+                        else
+                        {
+                            listBox2.Items.Add("Bu facebook Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut");
+                        }
+                    }
+                    else if (dgr == "https://www.instagram.com/")
+                    {
+                        urlac(urller[i].ToString() + listBox1.Items[j].ToString());
+                        if (drv.PageSource.IndexOf("Üzgünüz, bu sayfaya ulaşılamıyor.") != -1)
+                        {
+                            listBox2.Items.Add("Bu instagram Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut Değil..");
+                        }
+                        else
+                        {
+                            listBox2.Items.Add("Bu instagram Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut");
+                        }
+                    }
+                    else if (dgr == "https://github.com/")
+                    {
+                        urlac(urller[i].ToString() + listBox1.Items[j].ToString());
+                        if (drv.PageSource.IndexOf("Find code, projects, and people on GitHub:") != -1)
+                        {
+                            listBox2.Items.Add("Bu github Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut Değil..");
+                        }
+                        else
+                        {
+                            listBox2.Items.Add("Bu github Kullanıcı Adı : " + listBox1.Items[j].ToString() + " Mevcut");
+                        }
+                    }
+                }
+                
+            }
+
+        }
+
+        private void urlac(string url)
+        {
+            drv.Navigate().GoToUrl(url);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BastamaTextSave.txt_save.txtlistsave(listBox2, "Veriler Akatarıldı....", Application.StartupPath + "\\", "osint");
+        }
+    }
+}
